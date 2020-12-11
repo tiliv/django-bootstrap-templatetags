e99ad8a55fdf7ee053a906e0f74233c6868fd523
Steven K <steven@pointcircle.com>
2020-12-10 17:06:58 -0700

    Get everything else bolted in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-bootstrap-templatetags',
-      version='2.1.2',
+      version='2.1.3',
       description='Vanilla Bootstrap structures in simple rendering blocks.',
       author='Autumn Valenta',
       author_email='steven@pivotal.energy',
