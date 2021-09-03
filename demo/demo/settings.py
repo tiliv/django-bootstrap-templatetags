ba52b58faadb5715cfc476b50f71e906aa2a1239
Steven K <steven@pointcircle.com>
2021-09-03 11:17:08 -0700

    Test

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -57,7 +57,6 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
-        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
