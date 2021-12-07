fb2375032de7636f19b728666d04261494f4063b
Steven K <steven@pointcircle.com>
2021-12-07 20:44:16 +0000

    Version Bump [skip ci]

--- bootstrap_templatetags/__init__.py
+++ bootstrap_templatetags/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __name__ = "bootstrap_templatetags"
 __author__ = "Pivotal Energy Solutions"
-__version_info__ = (2, 1, 8)
-__version__ = "2.1.8"
+__version_info__ = (2, 1, 9)
+__version__ = "2.1.9"
 __date__ = "2013/07/8 9:47:00 AM"
 
 __credits__ = ["Tim Valenta", "Steven Klass"]
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-bootstrap-templatetags",
-    version="2.1.8",
+    version="2.1.9",
     description="Vanilla Bootstrap structures in simple rendering blocks.",
     author="Autumn Valenta",
     author_email="steven@pivotal.energy",
